32f58873ddb1633616240a761cc07ed634ac5889
Anvesh-123-p <64833339+Anvesh-123-p@users.noreply.github.com>
2020-07-26 22:04:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def predict():
     int_features=[int(x) for x in request.form.values()]
     final_features=[np.array(int_features)]
     pred=model.predict(final_features)
-    return render_template('index.html',prediction_text='the custme')
+    return render_template('index.html')
 
 if __name__ =="__main__":
     app.run(debug=True)
