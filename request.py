d7ba383607389decbdc93e35139c956493a6d538
Anvesh-123-p <64833339+Anvesh-123-p@users.noreply.github.com>
2020-07-26 20:28:37 +0530

    Update request.py

--- request.py
+++ request.py
@@ -2,6 +2,6 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
+r = requests.post(url,json={'Annual Income':2, 'Spending Score':9})
 
 print(r.json())
