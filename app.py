765703c76a4f8de31e100d24db02eb5d56a446a8
Anvesh-123-p <64833339+Anvesh-123-p@users.noreply.github.com>
2020-07-26 21:58:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def predict():
     int_features=[int(x) for x in request.form.values()]
     final_features=[np.array(int_features)]
     pred=model.predict(final_features)
-    return render_template('index.html',prediction_text='the custmer belongs to cluster {}'.format(pred))
+    return render_template('index.html',prediction_text='the custme'')
 
 if __name__ =="__main__":
     app.run(debug=True)
